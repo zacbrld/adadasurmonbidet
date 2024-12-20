b0d96e73b1755f4d16e20def1c75f8564392f750
zacbrld <zacharie.bourlard@epfl.ch>
2024-12-21 00:31:16 +0100

    biubi

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+  document.querySelectorAll('.tab-wrapper').forEach(wrapper => {
+    const buttons = wrapper.querySelectorAll('.tab-button');
+    const tabs = wrapper.querySelectorAll('.plot-container');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        tabs.forEach(tab => tab.classList.remove('active'));
+  
+        button.classList.add('active');
+        wrapper.querySelector(`#${button.dataset.target}`).classList.add('active');
+      });
+    });
+  });
